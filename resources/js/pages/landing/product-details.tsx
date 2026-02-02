4e18dd39cd7e8df7d7e30d746752686e30e7c51f
Min Htut <minhtutzaw16@gmail.com>
2026-02-02 21:02:16 +0630

    Remove wishlist button from product details page for a cleaner UI

--- resources/js/pages/landing/product-details.tsx
+++ resources/js/pages/landing/product-details.tsx
@@ -238,22 +238,7 @@ export default function ProductDetails({ product, auth }: Props) {
                                         : "Add to Cart"}
                                 </button>
 
-                                {/* Wishlist Button */}
-                                <button className="px-6 py-4 rounded-lg border-2 border-gray-300 hover:border-gray-900 transition">
-                                    <svg
-                                        className="h-6 w-6"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth={2}
-                                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                                        />
-                                    </svg>
-                                </button>
+                              
                             </div>
 
                             {/* Product Details */}
